095f6bfcb0f6f96c81391aa02bd0c555a4663e4d
Jason Evans <evans022@gmail.com>
2025-02-22 17:20:50 -0600

    remove pest test.

--- tests/Feature/User/RegisterUser.php
+++ tests/Feature/User/RegisterUser.php
@@ -5,15 +5,15 @@ use function Pest\Gwt\scenario;
 use function PHPUnit\Framework\assertEquals;
 use App\Livewire\Actions\Logout;
 
-scenario('User registers with valid data')
-    ->given(function () {
-        // I am logged out
-        // laravel breeze logout test
-        \Livewire\Livewire::test(Logout::class);
-    })
-    ->when(function () {
-        // I sign up with valid user data
-    })
-    ->then(function () {
-        // I should see a successful user registration message
-    });
+//scenario('User registers with valid data')
+//    ->given(function () {
+//        // I am logged out
+//        // laravel breeze logout test
+//        \Livewire\Livewire::test(Logout::class);
+//    })
+//    ->when(function () {
+//        // I sign up with valid user data
+//    })
+//    ->then(function () {
+//        // I should see a successful user registration message
+//    });
